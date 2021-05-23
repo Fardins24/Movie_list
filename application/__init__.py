72220958d1b4765e0c1290865ad8bd1dda34eeb1
Fardins24 <fshah@qa.com>
2021-05-24 00:03:41 +0100

    env

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@35.242.141.175:3306/movie_list"
-app.config['SECRET_KEY'] = "Fardin"
+app.config['SECRET_KEY'] = getenv("SECRETKEY")
 
 db = SQLAlchemy(app)
 
